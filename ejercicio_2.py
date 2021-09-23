21926d9c06b821f0f1f4e97c71034b97538b04e5
Sergio Clara <pscr.270390@gmail.com>
2021-09-22 21:22:10 -0500

    Ejercicio N° 2

---
+++ ejercicio_2.py
@@ -0,0 +1,11 @@
+
+
+
+nombre = "Marcos"
+apellido = "Clara"
+
+nombre_completo_f_string = f"{nombre} {apellido} " 
+
+print(nombre_completo_f_string)
+print(nombre_completo_f_string.lower())
+print(nombre_completo_f_string.upper())
