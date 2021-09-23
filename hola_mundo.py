8749076a385eb65a88f7bfec392c3d1ef4225506
Sergio Clara <pscr.270390@gmail.com>
2021-09-22 20:56:42 -0500

    Mi primer programa

---
+++ hola_mundo.py
@@ -0,0 +1,35 @@
+
+# message1 = "hola mundo 💙💙"
+# print(message1)
+
+# message2 = "Adios mundo"
+# print(message2)
+
+
+
+# print(message2.upper())
+# print(message2.lower())
+
+
+
+#nombre1 = "Paulo"
+#nombre2 = "Sergio"
+#apellido1 = "Clara"
+#apellido2 = "Ruiz"
+
+#nombre_completo = nombre1 + " " + nombre2 + " " + apellido1 + " " + apellido2
+
+#print(nombre_completo)
+
+#nombre_completo_f_string = f"{nombre1} {nombre2} {apellido1} {apellido2}"
+#print (nombre_completo_f_string)
+
+
+
+#texto = "uno\ndos\ntres"
+
+#print (texto)
+
+mi_mensaje = "        Hola         "
+print(mi_mensaje.strip())
+
