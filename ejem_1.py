08ce064eb5e1ecf2031ba04885b92e42833de515
Sergio Clara <pscr.270390@gmail.com>
2021-09-23 19:03:35 -0500

    Rubicando ejercicios

---
+++ ejem_1.py
@@ -0,0 +1,10 @@
+#Tarea 1-3
+#Busca una frase de alguien famoso que admires. Crea un programa que imprima el mensaje y el nombre del autor. 
+# La salida del programa debe ser algo como lo siguiente (incluyendo las comillas).
+# Albert Einstein una vez dijo: "Una persona que nunca cometió un error, nunca intentó algo nuevo".
+
+autor= "Tío Iroh"
+frase= "MUCHAS COSAS QUE PARECEN AMENAZANTES EN LA OSCURIDAD, SE VUELVEN AGRADABLES CUANDO LAS ILUMINAMOS"
+
+
+print("\nEl " + autor + " dijo " + '"' + frase + '"\n')
