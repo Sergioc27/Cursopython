0d5a6b11b90b9dba8cf553e2ad16ff17af61e964
Sergio Clara <pscr.270390@gmail.com>
2021-10-07 09:44:14 -0500

    correción ejercicio 2

--- Ejercicio_lista/Ejercicios_practica/Ejercicio_2_mensaje.py
+++ Ejercicio_lista/Ejercicios_practica/Ejercicio_2_mensaje.py
@@ -3,13 +3,13 @@
 nombres = [ 'Alfredo', 'Lupita', 'Brianda', 'Fernando', 'Uriel', 'Sergio']
 
 print ( " ")
-print ( f"Hola { nombres[0]} Espero que estes disfrutando tu cuso pyton ")
+print ( f"Hola { nombres[0]} Espero que estes disfrutando tu cuso python ")
 print ( " ")
-print ( f"Hola { nombres[1]} Espero que estes disfrutando tu cuso pyton ")
+print ( f"Hola { nombres[1]} Espero que estes disfrutando tu cuso python ")
 print ( " ")
-print ( f"Hola { nombres[2]} Espero que estes disfrutando tu cuso pyton ")
+print ( f"Hola { nombres[2]} Espero que estes disfrutando tu cuso pyhton ")
 print ( " ")
-print ( f"Hola { nombres[3]} Espero que estes disfrutando tu cuso pyton ")
+print ( f"Hola { nombres[3]} Espero que estes disfrutando tu cuso python ")
 print ( " ")
-print ( f"Hola { nombres[4]} Espero que estes disfrutando tu cuso pyton ")
+print ( f"Hola { nombres[4]} Espero que estes disfrutando tu cuso python ")
 print ( " ")
