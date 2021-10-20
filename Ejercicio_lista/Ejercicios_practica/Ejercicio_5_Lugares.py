0bcc9e5ef3ab4b0aaff46a661db9ce064de77bfc
Sergio Clara <pscr.270390@gmail.com>
2021-10-19 21:12:11 -0500

    nuevos ejercicios

---
+++ Ejercicio_lista/Ejercicios_practica/Ejercicio_5_Lugares.py
@@ -0,0 +1,25 @@
+
+#Parte 5.1 lista ordenada
+lugares = ["Japon", "Belgica", "Noruega", "Korea", "Rusia"]
+# print ("Original")
+# print (", ".join (lugares))
+# print("Lista ordenada")
+# print (", ".join (sorted( ["Japon", "Belgica", "Noruega", "Korea", "Rusia"])))
+# print ("lista sin modificar")
+# print (", ".join (lugares))
+
+#parte 5.2
+# print(", ".join(sorted(lugares, reverse = True)))
+# print (", ".join(lugares))
+#Parte 5.3
+# lugares.reverse()
+# print (", ".join (lugares))
+
+# lugares.reverse()
+# print(", ".join(lugares))
+
+lugares.sort(reverse= False)
+print(", ".join (lugares))
+
+lugares.sort(reverse = True)
+print(", ".join(lugares))
