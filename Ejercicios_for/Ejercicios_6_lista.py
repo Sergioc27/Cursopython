8a800c9e6a0ecc7a4478a363d580ebe6c614e492
Sergio Clara <pscr.270390@gmail.com>
2021-12-08 21:26:15 -0600

    Ejercicios 4ta sesion

--- Ejercicios_for/Ejercicios_6_lista.py
+++ Ejercicios_for/Ejercicios_6_lista.py
@@ -1,4 +1,29 @@
 #6.1 Lista del 0 al 10
 
-digits = list(range(11))
-print (digits)
+lista =[]
+contador = 11
+
+while contador > 0:
+    lista.insert (0, contador -1)
+    contador = contador-1
+
+print(f'Lista Ascendente: {lista}' )
+
+#6.2 Invertir lista sin la función reverse
+contador = 1
+lista = []
+for contador in range(0,11):
+    lista.insert(0,contador)
+
+print(f"Lista Descendente: {lista}" )
+
+
+#6.3 Lista Inversa
+
+lista.reverse()
+print(f'Lista Inversa {lista}')
+
+
+
+
+
